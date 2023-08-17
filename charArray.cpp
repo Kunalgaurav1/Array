79608514822bdc7c367b7ac09bf067450c22354d
kunalgaurav <kunalgaurav1420@gmail.com>
2023-08-17 09:08:54 +0530

    char array basci

---
+++ charArray.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char name[100];
+    
+    cout << "Enter you name : " << endl;
+    cin >> name;
+
+    cout << "Your name is " << name << endl;
+
+    return 0;
+}
